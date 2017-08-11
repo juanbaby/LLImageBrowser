fecead5205a23b8fad246935620da5d3a217ef0f
LL <763184581@qq.com>
2017-08-11 19:56:22 +0800

    have Done

--- LLImageBrowser/LLImageBrowser/BaseLayout.m
+++ LLImageBrowser/LLImageBrowser/BaseLayout.m
@@ -46,24 +46,24 @@
     
     CGPoint targetP = [super targetContentOffsetForProposedContentOffset:proposedContentOffset withScrollingVelocity:velocity];
     
-    NSArray *attrsAry = [super layoutAttributesForElementsInRect:CGRectMake(targetP.x, 0, self.collectionView.frame.size.width, 5000)];
+    NSArray *attrsAry = [super layoutAttributesForElementsInRect:CGRectMake(targetP.x, 0, self.collectionView.bounds.size.width, MAXFLOAT)];
     CGFloat max = MAXFLOAT;
    
     for( UICollectionViewLayoutAttributes *attrs in attrsAry  ){
-        
-        CGFloat reduceLength =((attrs.center.x-self.collectionView.contentOffset.x-self.collectionView.frame.size.width/2));
+        CGFloat reduceLength =((attrs.center.x-targetP.x-self.collectionView.frame.size.width*0.5));
         NSLog(@"reduceLength = %f",reduceLength);
         if (fabs(max)>fabs(reduceLength)) {
             max = reduceLength;
         }
         
-        
-        
-        
 
     }
     
     targetP.x += max;
+    NSLog(@"%f",targetP.x); 
+    if (targetP.x <0) {
+        targetP.x = 0;
+    }
     
    
 
@@ -71,14 +71,48 @@
     return targetP;
     
     
+//    // 拖动比较快 最终偏移量 不等于 手指离开时偏移量
+//    CGFloat collectionW = self.collectionView.bounds.size.width;
+//    
+//    // 最终偏移量
+//    CGPoint targetP = [super targetContentOffsetForProposedContentOffset:proposedContentOffset withScrollingVelocity:velocity];
+//    
+//    // 0.获取最终显示的区域
+//    CGRect targetRect = CGRectMake(targetP.x, 0, collectionW, MAXFLOAT);
+//    
+//    // 1.获取最终显示的cell
+//    NSArray *attrs = [super layoutAttributesForElementsInRect:targetRect];
+//    
+//    // 获取最小间距
+//    CGFloat minDelta = MAXFLOAT;
+//    for (UICollectionViewLayoutAttributes *attr in attrs) {
+//        // 获取距离中心点距离:注意:应该用最终的x
+//        CGFloat delta = (attr.center.x - targetP.x) - self.collectionView.bounds.size.width * 0.5;
+//        
+//        if (fabs(delta) < fabs(minDelta)) {
+//            minDelta = delta;
+//        }
+//    }
+//    
+//    // 移动间距
+//    targetP.x += minDelta;
+//    
+//    if (targetP.x < 0) {
+//        targetP.x = 0;
+//    }
+//    
+//    return targetP;
+
+    
+    
     
 }
 
 
-- (CGSize)collectionViewContentSize {
-//    NSLog(@"size = %@",NSStringFromCGSize([super collectionViewContentSize]));
-    return [super collectionViewContentSize];
-}
+//- (CGSize)collectionViewContentSize {
+////    NSLog(@"size = %@",NSStringFromCGSize([super collectionViewContentSize]));
+//    return [super collectionViewContentSize];
+//}
 
 
 @end
